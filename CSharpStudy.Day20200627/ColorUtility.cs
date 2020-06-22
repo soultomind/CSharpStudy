b054526406af235e28106840f53cdf67be9f1829
soultomind <soultomind@naver.com>
2020-06-22 22:11:26 +0900

    단순 - 발표 자료 작성 중

---
+++ CSharpStudy.Day20200627/ColorUtility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Day20200627
+{
+    class ColorUtility
+    {
+        public static Color FromArgb(int argb)
+        {
+            int alapha = (argb >> 24) & 0xFF;
+            int red = (argb >> 16) & 0xFF;
+            int green = (argb >> 8) & 0xFF;
+            int blue = argb & 0xFF;
+            return Color.FromArgb(alapha, red, green, blue); ;
+        }
+
+        public static int ToCssRgba(string argb)
+        {
+            if (argb == null)
+            {
+                throw new ArgumentNullException("argb");
+            }
+
+            if (argb.Length == 0)
+            {
+                throw new ArgumentException("argb.Length is zero");
+            }
+
+            if (!argb.StartsWith("rgba("))
+            {
+                throw new ArgumentException("argb is invalid value, usage is rgba(255,0,0,0)");
+            }
+
+            if (!argb.EndsWith(")"))
+            {
+                throw new ArgumentException("argb is invalid value, usage is rgba(255,0,0,0)");
+            }
+
+            argb = argb.Substring(5, argb.Length - 6);
+            string[] arrArgb = argb.Split(',');
+            if (arrArgb.Length != 4)
+            {
+                throw new ArgumentException("argb is invalid value, usage is rgba(255,0,0,0)");
+            }
+
+            int red = int.Parse(arrArgb[0]);
+            int green = int.Parse(arrArgb[1]);
+            int blue = int.Parse(arrArgb[2]);
+            int alpha = int.Parse(arrArgb[3]);
+            alpha = alpha << 24;
+            red = red << 16;
+            green = green << 8;
+            return alpha + red + green + blue;
+        }
+    }
+}
---
+++ CSharpStudy.Day20200627/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Day20200627
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 특정 구간에 대한 성능을 측정할 때 많이 사용된다.
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            ExamString();
+            Thread.Sleep(1500);
+
+            sw.Stop();
+            Console.WriteLine("Stopwatch.ElapsedMilliseconds={0}", sw.ElapsedMilliseconds / 1000);
+            Console.ReadKey();
+        }
+
+        private static void ExamDateTime()
+        {
+            DateTime newDateTime = new DateTime(2020, 06, 27, 10, 0, 0);
+            Console.WriteLine("CSharp Study Time={0}", newDateTime);
+
+            DateTime now = DateTime.Now;
+            DateTime utcNow = DateTime.UtcNow;
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            Console.WriteLine("1년중 {0}일이 지나가고 있습니다.", now.DayOfYear);
+            Console.WriteLine("오늘은 {0}.", now.DayOfWeek);
+
+            Console.WriteLine("다음주 스터디는 {0}일 입니다.", now.AddDays(7));
+            foreach (string fileName in Directory.GetFiles("C:\\Temp1"))
+            {
+                FileInfo fi = new FileInfo(fileName);
+                DateTime fileWriteTime = fi.LastWriteTime;
+
+                // 1초(Second)는 1,000 밀리초(Millisecond)인데 이보다 정밀도가 높은 시간 값이 필요하다면 Ticks 속성을 이용
+                // 1밀리초의 10,000분의 1에 해당하는 정밀도를 가지고 있다.
+                TimeSpan time = new TimeSpan(now.Ticks - fileWriteTime.Ticks);
+
+                if (time.TotalMinutes > 5)
+                {
+                    fi.Delete();
+                    Console.WriteLine("{0} 파일이 삭제되었습니다.", fi.Name);
+                }
+            }
+
+            DateTime birthTime = new DateTime(1987, 9, 30);
+
+            TimeSpan lifeTime = now.Subtract(birthTime);
+            Console.WriteLine("LifeTime.Days={0}", lifeTime.Days);
+
+            // 9시간 차이
+            Console.WriteLine("DateTime.UtcNow={0}, Kind={1}", utcNow, utcNow.Kind);
+            Console.WriteLine("DateTime.Now={0}, Kind={1}", now, now.Kind);
+
+            TimeSpan gap = new DateTime(now.Year, 12, 31) - now;
+            Console.WriteLine("올해 몇일이 남았나요?={0}", (int)gap.TotalDays);
+            Console.WriteLine("Now.DayOfYear + Gap.Totaldays = {0}", now.DayOfYear + (int)gap.TotalDays);
+        }
+
+        private static void ExamString()
+        {
+            // Alpha, Red, Green, Blue
+
+            // 8비트 8자리 1,2,4,8,16,32,64,128=255 값으로 ARGB 의 값을 Int 형으로 표현 할 수 있다.
+            int argb = 0x00FF0000;
+            Console.WriteLine("Argb=" + argb);
+            // Alapha=00000000, Red=11111111, Green=00000000, Blue=00000000
+
+            Color color = ColorUtility.FromArgb(argb);
+            Console.WriteLine(color.ToString());
+            Console.WriteLine("Color.ToArgb()={0}", color.ToArgb());
+
+            argb = ColorUtility.ToCssRgba("rgba(255, 0, 0, 0)");
+            Console.WriteLine("ColorUtility.ToCssRgba(\"rgba(255, 0, 0, 0\")={0}", argb);
+
+            string studyName = "C# Study";
+            if (studyName.IndexOf("") != -1)
+            {
+                Console.WriteLine("Study!");
+            }
+
+            // StringComparison.OrdinalIgnoreCase 대소문자 구문 X
+            // Equals, StartsWith, EndsWith, IndexOf ..
+
+            // {1} 부터 시작 할경우 FormatException 발생
+            string message = String.Format("{0} {1}", "C#", "Study");
+            Console.WriteLine(message);
+
+            // String 타입은 불변 객체 이기 때문에 모든 변환은 새로운 메모리 할당을 발생
+
+            // Capacity는 기존 Capacity 만큼의 문자열이 찰 경우 해당 크기의 두 배로!
+            StringBuilder builder = new StringBuilder();
+            Console.WriteLine("Capacity={0}", builder.MaxCapacity);
+            Console.WriteLine("Capacity={0}, Length={1}", builder.Capacity, builder.Length);
+            builder.Append("ABCDEFGHIJKLMNOP");
+            Console.WriteLine("Capacity={0}, Length={1}", builder.Capacity, builder.Length);
+            builder.Append("Q");
+            Console.WriteLine("Capacity={0}, Length={1}", builder.Capacity, builder.Length);
+            builder.Append(builder.ToString());
+            Console.WriteLine("Capacity={0}, Length={1}", builder.Capacity, builder.Length);
+        }
+    }
+}
