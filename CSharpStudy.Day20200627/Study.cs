86f2457b3da3e84e94fc985c86106cae7d91f13f
soultomind <soultomind@clipsoft.co.kr>
2020-06-26 16:42:43 +0900

    단순 - 작업 완료 ( 정리 필요 )

---
+++ CSharpStudy.Day20200627/Study.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Day20200627
+{
+    [Serializable]
+    public class Study
+    {
+        string name;
+
+        [NonSerialized]
+        string location;
+
+        public Study(string name, string location)
+        {
+            this.name = name;
+            this.location = location;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name={0}, Location={1}", name, location);
+        }
+    }
+}
