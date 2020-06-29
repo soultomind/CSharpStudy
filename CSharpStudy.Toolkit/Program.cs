ec9edc937e009d0165fe343e232fdbb09978ba1e
soultomind <soultomind@naver.com>
2020-06-29 21:41:01 +0900

    단순 - 툴킷 프로젝트 추가

---
+++ CSharpStudy.Toolkit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Toolkit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStart(@"C:\Workspace\DotNet\GitProject\CSharpStudy\CSharpStudy.Day20200704\bin\Debug\CSharpStudy.Day20200704.exe");
+        }
+
+        private static void ProcessStart(string fileName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(fileName);
+            //psi.WorkingDirectory = Path.GetDirectoryName(fileName);
+            Process.Start(psi);
+        }
+    }
+}
